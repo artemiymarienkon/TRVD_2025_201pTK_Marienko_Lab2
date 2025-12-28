6b30cd34c4c4685d8c50c3a494d3c11eee4061d9
artemiymarienkon <artemiymarie@gmail.com>
2025-12-28 19:33:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,31 +5,22 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// Шаблонізатор
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
-// Парсер форм
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Контролери
 const userController = require('./controllers/userController');
-const articleController = require('./controllers/articleController');
 
-// Маршрути
-app.get('/', (req, res) => res.render('index', { title: 'Головна' }));
-app.get('/about', (req, res) => res.render('about', { title: 'About' }));
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Open Encyclopedia' });
+});
 
 // Users
 app.get('/users', userController.getUsers);
 app.post('/users', userController.createUser);
 
-// Articles
-app.get('/articles', articleController.getArticles);
-app.post('/articles', articleController.createArticle);
-
-// Сервер
 app.listen(port, () => {
     console.log(`Сервер запущено: http://localhost:${port}`);
 });
